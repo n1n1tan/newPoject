59a4a87d80e158bcc3ded7b004e4f1effe687292
diar3 <arseniy.apelsinov@gmail.com>
2024-07-26 13:40:28 +0300

    test 1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#работа с изображениями
 import cv2
 import os
 img = cv2.imread('./data/road-signs/approaching-a-pedestrian-crossing.jpg')
@@ -31,9 +32,16 @@ if img is None:
 # new = (img.item(x, y, 0), img.item(x, y, 1), img.item(x, y, 2))
 
 
-x,x_max,y,y_max = map(int, input("введи координаты:").split())
+# конвертация в чернобелые оттенки
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('img', gray_image)
+
+
+# брезка изоьражения по координатам
+x,x_max,y,y_max = map(int, input("введи координаты:").split())
 cv2.imshow('img', img[x:x_max, y:y_max])
+
+#закрывает картинку если нажать esc
 while True:
     key = cv2.waitKey(1)
     if key == 27:
