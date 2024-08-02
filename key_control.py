b47a60ebc530fa9e61203b57b699a11a1a87155e
diar3 <arseniy.apelsinov@gmail.com>
2024-08-02 13:49:21 +0300

    Работает,

--- key_control.py
+++ key_control.py
@@ -9,57 +9,57 @@ q = Queue()
 
 
 def control_car():
-    per = 2
+    per = 4
     while (func := q.get()) is not None:
-        key_down(SC_UP)
-        # cof = func / 2000
-          # speed = func / 3400
-        # print(f'{func} --- {abs(cof):.3}')
+        print(per)
+
+
 
         LIMIT2 = 200
         LIMIT1 = 100
         LIMIT3 = 50
-        if per > 4:
-            key_press(SC_DEL)
-            per = 4
-        if per < 2:
-            key_press(SC_INS)
-            per = 2
+
+
+        if per > 3:
+            key_press(SC_DEL,interval=0.1)
+            per = 3
+
+
+        if per < 1:
+            key_press(SC_INS,interval=0.1)
+            per = 1
+
+
         if -1*LIMIT2 < func < LIMIT2:
-            ...
             key_up(SC_LEFT)
             key_up(SC_RIGHT)
-            key_press(SC_INS)
-            if per < 3:
-                key_press(SC_INS)
-                per = 4
+            key_down(SC_UP)
+            if per != 3 and per < 4:
+                key_press(SC_INS, interval=0.1)
+                per = per + 1
+
+
         elif func > LIMIT2:
             key_down(SC_RIGHT)
-            key_press(SC_DEL)
-            if per != 2:
-                key_press(SC_DEL)
-                per = 2
-        elif LIMIT3 < func < LIMIT1:
+            key_up(SC_UP)
+            if per != 1:
+                key_press(SC_DEL,interval=0.1)
+                per = per - 1
 
-            # key_press(SC_UP, interval=0.02)
+
+        elif LIMIT3 < func < LIMIT1:
             key_press(SC_LEFT, interval=0.03)
-            # key_press(SC_LEFT, interval=abs(cof) / 2)
-            # if func > 270:
-            #     key_press(SC_DOWN, interval=0.09)
+
+
         elif -1*LIMIT1 < func < -1*LIMIT3:
             key_press(SC_RIGHT, interval=0.03)
 
         elif func < LIMIT2:
             key_down(SC_LEFT)
-            if per != 2:
-                key_press(SC_DEL)
-                per = per - 2
-            #key_press(SC_RIGHT, interval=0.05)
-            # key_press(SC_UP,interval = 0.06)
-            # key_press(SC_LEFT, interval=abs(cof))
-            # key_press(SC_RIGHT, interval=abs(cof) / 2)
-            # if func < -270:
-            #     key_press(SC_DOWN, interval=0.09)
-            #     key_press(SC_LEFT, interval=abs(cof) )
+            key_up(SC_UP)
+            if per != 1:
+                key_press(SC_DEL,interval=0.1)
+                per = per - 1
+
 
 th = Thread(target=control_car)
